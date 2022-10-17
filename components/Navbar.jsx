68dd6021a682de06ca7aa60ab6530769c64df659
Sean Xavier Nieva <61405165+iliwysfyasbysuoi@users.noreply.github.com>
2022-10-17 19:37:53 +0800

    removed default html contents

---
+++ components/Navbar.jsx
@@ -0,0 +1,21 @@
+
+
+const Navbar = () =>{
+    return(
+        <>
+            <nav>
+                <div className="logo">
+                    <h1>Movie Rater</h1>
+                </div>
+                <a href="">Home</a>
+                <a href="">Movies</a>
+                <a href="">TV Shows</a>
+                <a href="">About</a>
+
+            </nav>
+
+        </>
+    )
+}
+
+export default Navbar;

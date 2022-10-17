181127449640f48c03979b6c410993fbd97f316c
Sean Xavier Nieva <61405165+iliwysfyasbysuoi@users.noreply.github.com>
2022-10-17 20:27:24 +0800

    setup navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,17 +1,21 @@
 
 
-const Navbar = () =>{
-    return(
+const Navbar = () => {
+    return (
         <>
-            <nav>
-                <div className="logo">
-                    <h1>Movie Rater</h1>
-                </div>
-                <a href="">Home</a>
-                <a href="">Movies</a>
-                <a href="">TV Shows</a>
-                <a href="">About</a>
-
+            <nav className="bg-coolgray-100 h-20  m-0 p-0 flex  flex-row items-center justify-between w-full fixed ">
+                    <div className=" h-20  flex items-center">
+                        <h1 className="navlink">Movie Rater</h1>
+                    </div>
+                    <div className=" h-20 flex m-0 p-0 items-center ">
+                        <a href="" className="navlink">Home</a>
+                        <a href="" className="navlink">Movies</a>
+                        <a href="" className="navlink">TV Shows</a>
+                        <a href="" className="navlink">About</a>
+                    </div>
+                    <div className=" h-20  flex items-center">
+                        <a href="" className="navlink">Profile</a>
+                    </div>
             </nav>
 
         </>
--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
 
 
 
-      <main >
+      <main className=''>
         
       </main>
 
